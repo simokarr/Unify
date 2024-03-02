263732d7da2731936c2b393be8d97394e2acaef4
LJulien27 <ljuli048@uOttawa.ca>
2024-03-02 05:25:45 -0500

    Added map to tab 1

--- Unify/unify/app/(tabs)/index.tsx
+++ Unify/unify/app/(tabs)/index.tsx
@@ -3,12 +3,20 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
+import MapView from 'react-native-maps'; // Import MapView
+
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 45.4215,
+          longitude: -75.6826,
+          latitudeDelta: 0.01, // Adjust the zoom level as needed
+          longitudeDelta: 0.01,
+        }}
+      />
     </View>
   );
 }
@@ -19,13 +27,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
