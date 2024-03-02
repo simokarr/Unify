dc8e8f4d9340caa2e95ed52e6bc7aff04503105d
Youssef Mroue <mroueh.youssef10@gmail.com>
2024-03-02 17:37:41 -0500

    small updates

--- Unify/unify/app/(tabs)/two.tsx
+++ Unify/unify/app/(tabs)/two.tsx
@@ -7,7 +7,7 @@ import React from 'react';
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>Profile</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
